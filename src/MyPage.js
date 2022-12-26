2661aa4eff1d8536095bc13a46e959a5dd44c9bb
Tausif4171 <tausifkhan4171@gmail.com>
2022-12-26 17:24:13 +0530

    hubspot form error fixed using useEffect hook

--- src/MyPage.js
+++ src/MyPage.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { useHubspotForm } from "@aaronhayes/react-use-hubspot-form";
 
 function MyPage(props) {
-  const { loaded, error, formCreated } = useHubspotForm({
-    portalId: "23633466",
-    formId: "5641b914-ab3b-4e0d-a6de-a1869c557d6c",
-    target: "#my-hubspot-form",
-  });
+//   const { loaded, error, formCreated } = useHubspotForm({
+//     portalId: "23633466",
+//     formId: "5641b914-ab3b-4e0d-a6de-a1869c557d6c",
+//     target: "#my-hubspot-form",
+//   });
+
+useEffect(() => {
+    const script = document.createElement('script');
+    script.src='https://js.hsforms.net/forms/v2.js';
+    document.body.appendChild(script);
+
+    script.addEventListener('load', () => {
+        // @TS-ignore
+        if (window.hbspt) {
+            // @TS-ignore
+            window.hbspt.forms.create({
+                portalId: '23633466',
+                formId: '5641b914-ab3b-4e0d-a6de-a1869c557d6c',
+                target: '#my-hubspot-form'
+            })
+        }
+    });
+}, []);
 
   return ( 
     <div style={{margin:"0px 120px"}}>
