69249c04f01f439be53d76dcbb3fc187b454c112
Tausif4171 <tausifkhan4171@gmail.com>
2022-12-30 14:36:46 +0530

    button to show form

--- src/MyPage.js
+++ src/MyPage.js
@@ -1,34 +1,38 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import { useHubspotForm } from "@aaronhayes/react-use-hubspot-form";
 
 function MyPage(props) {
-//   const { loaded, error, formCreated } = useHubspotForm({
-//     portalId: "23633466",
-//     formId: "5641b914-ab3b-4e0d-a6de-a1869c557d6c",
-//     target: "#my-hubspot-form",
-//   });
+  // const { loaded, error, formCreated } = useHubspotForm({
+  //   portalId: "23633466",
+  //   formId: "5641b914-ab3b-4e0d-a6de-a1869c557d6c",
+  //   target: "#my-hubspot-form",
+  // });
+  const [show, setShow] = useState(false);
 
-useEffect(() => {
-    const script = document.createElement('script');
-    script.src='https://js.hsforms.net/forms/v2.js';
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://js.hsforms.net/forms/v2.js";
     document.body.appendChild(script);
 
-    script.addEventListener('load', () => {
+    if (show) {
+      script.addEventListener("load", () => {
         // @TS-ignore
         if (window.hbspt) {
-            // @TS-ignore
-            window.hbspt.forms.create({
-                portalId: '6485032',
-                formId: '9fc93abc-a8c7-48de-a6d5-2a3c1583e64f',
-                target: '#my-hubspot-form'
-            })
+          // @TS-ignore
+          window.hbspt.forms.create({
+            portalId: "6485032",
+            formId: "9fc93abc-a8c7-48de-a6d5-2a3c1583e64f",
+            target: "#my-hubspot-form",
+          });
         }
-    });
-}, []);
+      });
+    }
+  }, [show]);
 
-  return ( 
-    <div style={{margin:"0px 120px"}}>
-      <h1 style={{color:"#ff7a59"}}>Hubspot Form</h1>
+  return (
+    <div style={{ margin: "0px 120px" }}>
+      <h1 style={{ color: "#ff7a59" }}>Hubspot Form</h1>
+      <button onClick={() => setShow(true)}>CTAs</button>
       <div id="my-hubspot-form"></div>
     </div>
   );
